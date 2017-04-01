5e94ef9b1ded30a3cf836551c62cbe2fb0435bea
Ragnar Valgeirsson <ragnar.valgeirsson@gmail.com>
2017-04-01 19:34:37 +0000

    Update

--- src/App.js
+++ src/App.js
@@ -12,11 +12,11 @@ export default () => (
         <p>Currently, I'm doing cool things for the interwebs at <a href="https://aranja.com">Aranja</a> and organizing <a href="https://jsconf.is">JSConf Iceland</a>, which is super awesome! Coffee is my drug of choice and I tend to listen to the same music on repeat, for an extended period of time.</p>
 
         <h3>Question: Why is this website so… empty?</h3>
-        <p>Unfortunately, I don't have time to make it better <abbr title="at the moment">atm</abbr>. Perhaps, it will be super brilliant in the future, but ‘til then, find me online, at:</p>
+        <p>Unfortunately, I don't have time to make it better <abbr title="at the moment">atm</abbr>. Perhaps, it will be brilliant in the future, but ‘til then, find me online, at:</p>
         <ul>
           <li><a href="https://twitter.com/rthor">twitter</a></li>
           <li><a href="https://github.com/rthor">github</a></li>
-          <li><a href="https://linkedin.com/in/rthor">linkin</a></li>
+          <li><a href="https://linkedin.com/in/rthor">linkedin</a></li>
         </ul>
       </Container>
     </main>
